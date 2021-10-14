1003a5afe3c8c1859d731f665d401fca6ecda67c
Samuel <samlee1097@yahoo.com>
2021-10-13 20:16:28 -0400

    commit tree e718e81e3ff2e7651db2ac9e8a8241032c4f1372

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list)
+        list_of_words=[]
+        list.map do |check| 
+            if word.chars.sort == check.chars.sort
+                list_of_words << check
+            end
+        end
+        list_of_words
+    end
+end
